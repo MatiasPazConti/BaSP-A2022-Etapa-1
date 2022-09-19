7da90e9cdc43bcf78c9074a5d56d379c078ff11b
Matias Paz <matiaspazconti2020@gmail.com>
2022-09-19 16:48:01 -0300

    BaSP-W05-2022: Fixed some mistakes in exercise-01 and exercise-02

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,24 +1,24 @@
 console.log('--EXERCISE 01: VARIABLES AND OPERATORS');
 /*  Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de
     ambos números en una 3er variable.  */
-console.log('\n-Exercise 1.a: ');
+console.log('\n-Exercise 1.a:');
 var firstNumber, secondNumber, sumTotal;
 firstNumber = 10;
 secondNumber = 5;
 sumTotal = firstNumber + secondNumber;
-console.log(sumTotal);
+console.log('Sum:', sumTotal);
 /*  Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.  */
-console.log('\n-Exercise 1.b: ');
+console.log('\n-Exercise 1.b:');
 var firstString, secondString, concatenation;
-firstString = 'ho';
-secondString = 'la';
+firstString = 'hola';
+secondString = 'mundo';
 concatenation = firstString + secondString;
-console.log(concatenation);
+console.log('Concatenation:', concatenation);
 /*  Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del
     string) guardando el resultado de la suma en una 3er variable (utilizar length).    */
-console.log('\n-Exercise 1.c: ');
+console.log('\n-Exercise 1.c:');
 var thirdString, fourthString, totalLenght;
 thirdString = 'hola';
 fourthString = 'mundo';
-totalLenght = length(thirdString) + length(fourthString);
-console.log(totalLenght);
+totalLenght = thirdString.length + fourthString.length;
+console.log('Total lenght:', totalLenght);
--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -1,48 +1,50 @@
 console.log('--EXERCISE 02: STRINGS');
 /*  Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula
     (utilizar toUpperCase). */
-console.log('\n-Exercise 2.a: ');
+console.log('\n-Exercise 2.a:');
 var firstStringA = 'textodeprueba';
-console.log(firstStringA.toUpperCase());
+console.log('String:', firstStringA.toUpperCase());
 /*  Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros
     5 caracteres guardando el resultado en una nueva variable (utilizar substring). */
-console.log('\n-Exercise 2.b: ');
+console.log('\n-Exercise 2.b:');
 var firstStringB = 'textodeprueba';
-var firstSubstringB = firstStringB.substring(1, 5);
-console.log(firstSubstringB);
+var firstSubstringB = firstStringB.substring(0, 5);
+console.log('Sub-string:', firstSubstringB);
 /*  Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos
     3 caracteres guardando el resultado en una nueva variable (utilizar substring). */
-console.log('\n-Exercise 2.c: ');
+console.log('\n-Exercise 2.c:');
 var firstStringC = 'textodeprueba';
-var firstSubstringC = firstStringC.substring(length(firstStringC) - 3);
-console.log(firstSubstringC);
+var firstSubstringC = firstStringC.substring(firstStringC.length - 3);
+console.log('Sub-string:', firstSubstringC);
 /*  Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera
     letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar
     substring, toUpperCase, toLowerCase y el operador +).   */
-console.log('\n-Exercise 2.d: ');
+console.log('\n-Exercise 2.d:');
 var firstStringD = 'textodeprueba';
-var firstSubstringD = firstStringD.substring(1, 1);
-var secondSubstringD = firstStringD.substring(2);
+var firstSubstringD = firstStringD.substring(0, 1);
+var secondSubstringD = firstStringD.substring(1);
 var returnD = firstSubstringD.toUpperCase() + secondSubstringD.toLowerCase();
-console.log(returnD);
+console.log('String:', returnD);
 /*  Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la
     posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).   */
-console.log('\n-Exercise 2.e: ');
-var firstStringE = 'texto deprueba';
-var ocurrencePosE = firstStringE.indexOf();
-console.log(ocurrencePosE);
+console.log('\n-Exercise 2.e:');
+var firstStringE = 'texto de prueba';
+var ocurrencePosE = firstStringE.indexOf(' ');
+console.log('First blank-space position:', ocurrencePosE);
 /*  Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
     Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de
     ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase,
     toLowerCase y el operador +).   */
-console.log('\n-Exercise 2.f: ');
+console.log('\n-Exercise 2.f:');
 var firstStringF = 'textodeprueba holatodomundo';
-var ocurrencePosF = firstStringF.indexOf();
-var firstSubstringF = firstStringF.substring(1, 1);
-var secondSubstringF = firstStringF.substring(2, ocurrencePosF - 1);
-var thirdSubstringF = firstStringF.substring(ocurrencePosF + 1, ocurrencePosF + 1);
+var ocurrencePosF = firstStringF.indexOf(' ');
+//First part
+var firstSubstringF = firstStringF.substring(0, 1);
+var secondSubstringF = firstStringF.substring(1, ocurrencePosF);
+//Second part
+var thirdSubstringF = firstStringF.substring(ocurrencePosF + 1, ocurrencePosF + 2);
 var fourthSubstringF = firstStringF.substring(ocurrencePosF + 2);
-var returnF = firstSubstringF.toUpperCase() + secondSubstringF.toLowerCase() + ' ' + thirdSubstringF.toUpperCase()
-            + fourthSubstringF.toLowerCase();
-console.log(returnF);
-    
+//Concatenation
+var returnF = firstSubstringF.toUpperCase() + secondSubstringF.toLowerCase() + ' '
+            + thirdSubstringF.toUpperCase() + fourthSubstringF.toLowerCase();
+console.log('String:', returnF);
