a2b6bc53d6af5f34e7dbdfe0a280d19584e4a9b9
Matias Paz <matiaspazconti2020@gmail.com>
2022-09-20 20:37:25 -0300

    BaSP-W05-2022: Finished exercise-05

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -5,9 +5,8 @@ console.log('\n-Exercise 5.a:');
 var array = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
 var toDisplay = '';
 for (let i = 0; i < 5; i++) {
-    toDisplay = toDisplay + array[i] + ' ';
+    alert(toDisplay);
 }
-alert(toDisplay);
 console.log('Array:', array);
 /*  Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra
     modificada. */
@@ -23,7 +22,18 @@ console.log('Array:', array);
     bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con
     la cadena completa. */
 console.log('\n-Exercise 5.c:');
+var sentence = '';
+for (let i = 0; i < 5; i++) {
+    sentence = sentence + array[i];
+}
+alert(sentence);
+console.log('Array:', array);
 /*  Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es
     decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0
     hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).  */
 console.log('\n-Exercise 5.d:');
+var arrayD = [];
+for (let i = 0; i < 10; i++) {
+    arrayD.push(i);
+}
+console.log('Array:', arrayD);
