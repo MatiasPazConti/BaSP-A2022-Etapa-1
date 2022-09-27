745f1eb60a77bd96556842310d3b117dd9d6e2a6
Matias Paz <matiaspazconti2020@gmail.com>
2022-09-27 20:15:04 -0300

    BaSP-W06-2022: Added email and password requeriments in signup.js

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,3 +1,4 @@
+//  errorFocus FUNCTION
 function errorFocus(section) {
     var errorLabel = section.querySelector('.error-label');
     if (errorLabel != null) {
@@ -5,6 +6,7 @@ function errorFocus(section) {
         delete errorLabel;
     }
 }
+//  errorBlur FUNCTION
 function errorBlur(section, input) {
     var errorLabel = document.createElement('label');
     errorLabel.classList.add('error-label');
@@ -12,12 +14,85 @@ function errorBlur(section, input) {
         errorLabel.innerHTML = 'Required field';
         section.appendChild(errorLabel);
     }
+    else if (section.getAttribute('id') == 'fname-section')
+    {
+        //  First-Name
+    }
+    else if (section.getAttribute('id') == 'lname-section')
+    {
+        //  Last-Name
+    }
+    else if (section.getAttribute('id') == 'doc-section')
+    {
+        //  Documment
+    }
+    else if (section.getAttribute('id') == 'birth-section')
+    {
+        //  Birth-Date
+    }
+    else if (section.getAttribute('id') == 'phone-section')
+    {
+        //  Phone-Number
+    }
+    else if (section.getAttribute('id') == 'dir-section')
+    {
+        //  Direction
+    }
+    else if (section.getAttribute('id') == 'town-section')
+    {
+        //  Town
+    }
+    else if (section.getAttribute('id') == 'postal-section')
+    {
+        //  Postal-Code
+    }
+    else if (section.getAttribute('id') == 'email-section')
+    {
+        //  Email
+        var emailValidation = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+        if (!emailValidation.test(input.value)) {
+            errorLabel.innerHTML = 'Invalid email';
+            section.appendChild(errorLabel);
+        }
+        else {
+            delete errorLabel;
+        }
+    }
+    else if ((section.getAttribute('id') == 'pass-section') || (section.getAttribute('id') == 'rpass-section'))
+    {
+        //  Password
+        var passValidation = /^[a-z0-9]+$/i;
+        console.log(input.value);
+        console.log(input.value.length);
+        if ((input.value.length < 8) || (input.value.length > 16) || !passValidation.test(input.value))
+        {
+            errorLabel.innerHTML = 'Invalid password';
+            section.appendChild(errorLabel);
+        }
+        else {
+            delete errorLabel;
+        }
+    }
     else {
         delete errorLabel;
     }
 }
+//  errorRepeat FUNCTION
+function errorRepeat(section, rSection) {
+    var errorLabel = document.createElement('label');
+    errorLabel.classList.add('error-label');
+    errorLabel.setAttribute('id', 'repeat-label');
+    if (rSection.querySelector('input').value != section.querySelector('input').value) {
+        errorLabel.innerHTML = 'Password does not match';
+        rSection.appendChild(errorLabel);
+    }
+    else
+    {
+        delete errorLabel
+    }
+}
 window.onload = function () {
-    // First-Name Functions
+    // First-Name FOCUS and BLUR
     const fNameSection = document.getElementById('fname-section');
     const fNameInput = fNameSection.querySelector('input');
     fNameInput.onfocus = function() {
@@ -26,7 +101,7 @@ window.onload = function () {
     fNameInput.onblur = function() {
         errorBlur(fNameSection, fNameInput);
     }
-    //  Last-Name Functions
+    //  Last-Name FOCUS and BLUR
     const lNameSection = document.getElementById('lname-section');
     const lNameInput = lNameSection.querySelector('input');
     lNameInput.onfocus = function() {
@@ -35,7 +110,7 @@ window.onload = function () {
     lNameInput.onblur = function() {
         errorBlur(lNameSection, lNameInput);
     }
-    //  Documment Functions
+    //  Documment FOCUS and BLUR
     const docSection = document.getElementById('doc-section');
     const docInput = docSection.querySelector('input');
     docInput.onfocus = function() {
@@ -44,7 +119,7 @@ window.onload = function () {
     docInput.onblur = function() {
         errorBlur(docSection, docInput);
     }
-    //  Birth-Date Functions
+    //  Birth-Date FOCUS and BLUR
     const birthSection = document.getElementById('birth-section');
     const birthInput = birthSection.querySelector('input');
     birthInput.onfocus = function() {
@@ -53,7 +128,7 @@ window.onload = function () {
     birthInput.onblur = function() {
         errorBlur(birthSection, birthInput);
     }
-    //  Phone-Number Section
+    //  Phone-Number FOCUS and BLUR
     const phoneSection = document.getElementById('phone-section');
     const phoneInput = phoneSection.querySelector('input');
     phoneInput.onfocus = function() {
@@ -62,7 +137,7 @@ window.onload = function () {
     phoneInput.onblur = function() {
         errorBlur(phoneSection, phoneInput);
     }
-    //  Direction Section
+    //  Direction FOCUS and BLUR
     const dirSection = document.getElementById('dir-section');
     const dirInput = dirSection.querySelector('input');
     dirInput.onfocus = function() {
@@ -71,7 +146,7 @@ window.onload = function () {
     dirInput.onblur = function() {
         errorBlur(dirSection, dirInput);
     }
-    //  Town Section
+    //  Town FOCUS and BLUR
     const townSection = document.getElementById('town-section');
     const townInput = townSection.querySelector('input');
     townInput.onfocus = function() {
@@ -80,7 +155,7 @@ window.onload = function () {
     townInput.onblur = function() {
         errorBlur(townSection, townInput);
     }
-    //  Postal-Code Section
+    //  Postal-Code FOCUS and BLUR
     const postalSection = document.getElementById('postal-section');
     const postalInput = postalSection.querySelector('input');
     postalInput.onfocus = function() {
@@ -89,7 +164,7 @@ window.onload = function () {
     postalInput.onblur = function() {
         errorBlur(postalSection, postalInput);
     }
-    //  Email Section
+    //  Email FOCUS and BLUR
     const emailSection = document.getElementById('email-section');
     const emailInput = emailSection.querySelector('input');
     emailInput.onfocus = function() {
@@ -98,7 +173,7 @@ window.onload = function () {
     emailInput.onblur = function() {
         errorBlur(emailSection, emailInput);
     }
-    //  Password Section
+    //  Password FOCUS and BLUR
     const passSection = document.getElementById('pass-section');
     const passInput = passSection.querySelector('input');
     passInput.onfocus = function() {
@@ -106,8 +181,12 @@ window.onload = function () {
     }
     passInput.onblur = function() {
         errorBlur(passSection, passInput);
+        if (rPassSection.querySelector('#repeat-label') != null) {
+            errorFocus(rPassSection);
+        }
+        errorRepeat(passSection, rPassSection);
     }
-    //  Repeat-Password Section
+    //  Repeat-Password FOCUS and BLUR
     const rPassSection = document.getElementById('rpass-section');
     const rPassInput = rPassSection.querySelector('input');
     rPassInput.onfocus = function() {
@@ -115,5 +194,8 @@ window.onload = function () {
     }
     rPassInput.onblur = function() {
         errorBlur(rPassSection, rPassInput);
+        if (rPassSection.querySelector('.error-label') == null) {
+            errorRepeat(passSection, rPassSection);
+        }
     }
 }
