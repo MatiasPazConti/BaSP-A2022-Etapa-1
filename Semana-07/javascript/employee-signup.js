a1cea5edc6c144b03afe33ba010c8827c486332e
Matias Paz <matiaspazconti2020@gmail.com>
2022-10-04 17:19:26 -0300

    BaSP-W07-2022: Finished request functions in signup, fixed some feedback mistakes

--- Semana-07/javascript/employee-signup.js
+++ Semana-07/javascript/employee-signup.js
@@ -25,12 +25,8 @@ function errorBlur(section, input) {
                 for (i = 0; nameValidation && (i < input.value.length); i++){
                     nameValidation = (LETTERS.indexOf(input.value.charAt(i), 0) != -1) || (input.value.charAt(i) == EMPTYSPACE);
                 }
-                var firstSpace = input.value.indexOf(EMPTYSPACE, 0);
-                if (firstSpace != -1) {
-                    var invalidSpace = (firstSpace == 0) || (firstSpace == input.value.length - 1);
-                    var moreSpaces = input.value.indexOf(EMPTYSPACE, firstSpace + 1) != -1;
-                }
-                if ((input.value.length <= 3) || !nameValidation || ((firstSpace != -1) && (invalidSpace || moreSpaces))) {
+                var invalidSpace = input.value.indexOf(EMPTYSPACE, 0) != -1;
+                if ((input.value.length <= 3) || !nameValidation || invalidSpace) {
                     errorLabel.innerHTML = 'Invalid name';
                     section.appendChild(errorLabel);
                     return false;
@@ -62,7 +58,7 @@ function errorBlur(section, input) {
                 for (i = 0; phoneValidation && (i < input.value.length); i++) {
                     phoneValidation = NUMBERS.indexOf(input.value.charAt(i), 0) != -1;
                 }
-                if ((input.value.length <= 10) || !phoneValidation) {
+                if ((input.value.length != 10) || !phoneValidation) {
                     errorLabel.innerHTML = 'Invalid number';
                     section.appendChild(errorLabel);
                     return false;
@@ -165,8 +161,153 @@ function errorRepeat(section, rSection) {
         return true;
     }
 }
+function changeDateStructure(date) {
+    var dateArray = date.split('-');
+    return dateArray[1] + '/' + dateArray[2] + '/' + dateArray[0];
+}
+function adjustInput(section, input) {
+    if (section.querySelector('.error-label') != null) {
+        input.classList.add('error-input');
+    }
+    else if (section.querySelector('.error-input') != null) {
+        input.classList.remove('error-input');
+    }
+}
 window.onload = function () {
     var validations = [false, false, false, false, false, false, false, false, false, false, false];
+
+    const fNameSection = document.getElementById('fname-section');
+    const fNameInput = fNameSection.querySelector('input');
+    fNameInput.onfocus = function() {
+        errorFocus(fNameSection);
+        adjustInput(fNameSection, fNameInput);
+    }
+    fNameInput.onblur = function() {
+        validations[0] = errorBlur(fNameSection, fNameInput);
+        adjustInput(fNameSection, fNameInput);
+    }
+
+    const lNameSection = document.getElementById('lname-section');
+    const lNameInput = lNameSection.querySelector('input');
+    lNameInput.onfocus = function() {
+        errorFocus(lNameSection);
+        adjustInput(lNameSection, lNameInput);
+    }
+    lNameInput.onblur = function() {
+        validations[1] = errorBlur(lNameSection, lNameInput);
+        adjustInput(lNameSection, lNameInput);
+    }
+
+    const docSection = document.getElementById('doc-section');
+    const docInput = docSection.querySelector('input');
+    docInput.onfocus = function() {
+        errorFocus(docSection);
+        adjustInput(docSection, docInput);
+    }
+    docInput.onblur = function() {
+        validations[2] = errorBlur(docSection, docInput);
+        adjustInput(docSection, docInput);
+    }
+
+    const birthSection = document.getElementById('birth-section');
+    const birthInput = birthSection.querySelector('input');
+    birthInput.onfocus = function() {
+        errorFocus(birthSection);
+        adjustInput(birthSection, birthInput);
+    }
+    birthInput.onblur = function() {
+        validations[3] = errorBlur(birthSection, birthInput);
+        adjustInput(birthSection, birthInput);
+    }
+
+    const phoneSection = document.getElementById('phone-section');
+    const phoneInput = phoneSection.querySelector('input');
+    phoneInput.onfocus = function() {
+        errorFocus(phoneSection);
+        adjustInput(phoneSection, phoneInput);
+    }
+    phoneInput.onblur = function() {
+        validations[4] = errorBlur(phoneSection, phoneInput);
+        adjustInput(phoneSection, phoneInput);
+    }
+
+    const addressSection = document.getElementById('address-section');
+    const addressInput = addressSection.querySelector('input');
+    addressInput.onfocus = function() {
+        errorFocus(addressSection);
+        adjustInput(addressSection, addressInput);
+    }
+    addressInput.onblur = function() {
+        validations[5] = errorBlur(addressSection, addressInput);
+        adjustInput(addressSection, addressInput);
+    }
+
+    const townSection = document.getElementById('town-section');
+    const townInput = townSection.querySelector('input');
+    townInput.onfocus = function() {
+        errorFocus(townSection);
+        adjustInput(townSection, townInput);
+    }
+    townInput.onblur = function() {
+        validations[6] = errorBlur(townSection, townInput);
+        adjustInput(townSection, townInput);
+    }
+
+    const postalSection = document.getElementById('postal-section');
+    const postalInput = postalSection.querySelector('input');
+    postalInput.onfocus = function() {
+        errorFocus(postalSection);
+        adjustInput(postalSection, postalInput);
+    }
+    postalInput.onblur = function() {
+        validations[7] = errorBlur(postalSection, postalInput);
+        adjustInput(postalSection, postalInput);
+    }
+
+    const emailSection = document.getElementById('email-section');
+    const emailInput = emailSection.querySelector('input');
+    emailInput.onfocus = function() {
+        errorFocus(emailSection);
+        adjustInput(emailSection, emailInput);
+    }
+    emailInput.onblur = function() {
+        validations[8] = errorBlur(emailSection, emailInput);
+        adjustInput(emailSection, emailInput);
+    }
+
+    const passSection = document.getElementById('pass-section');
+    const passInput = passSection.querySelector('input');
+    passInput.onfocus = function() {
+        errorFocus(passSection);
+        adjustInput(passSection, passInput);
+        adjustInput(rPassSection, rPassInput);
+    }
+    passInput.onblur = function() {
+        validations[9] = errorBlur(passSection, passInput);
+        if (rPassSection.querySelector('#repeat-label') != null) {
+            errorFocus(rPassSection);
+        }
+        validations[10] = errorRepeat(passSection, rPassSection);
+        adjustInput(passSection, passInput);
+        adjustInput(rPassSection, rPassInput);
+    }
+
+    const rPassSection = document.getElementById('rpass-section');
+    const rPassInput = rPassSection.querySelector('input');
+    rPassInput.onfocus = function() {
+        errorFocus(rPassSection);
+        adjustInput(passSection, passInput);
+        adjustInput(rPassSection, rPassInput);
+    }
+    rPassInput.onblur = function() {
+        validations[10] = errorBlur(rPassSection, rPassInput);
+        if (rPassSection.querySelector('.error-label') == null) {
+            validations[9] = errorRepeat(passSection, rPassSection);
+        }
+        adjustInput(passSection, passInput);
+        adjustInput(rPassSection, rPassInput);
+    }
+
     var userPassword = localStorage.getItem('password');
     if (userPassword) {
         fNameInput.setAttribute('value', localStorage.getItem('firstName'));
@@ -181,112 +322,6 @@ window.onload = function () {
         passInput.setAttribute('value', userPassword);
         rPassInput.setAttribute('value', userPassword);
         validations = [true, true, true, true, true, true, true, true, true, true, true];
-    }
-
-    const fNameSection = document.getElementById('fname-section');
-    const fNameInput = fNameSection.querySelector('input');
-    fNameInput.onfocus = function() {
-        errorFocus(fNameSection);
-    }
-    fNameInput.onblur = function() {
-        validations[0] = errorBlur(fNameSection, fNameInput);
-    }
-
-    const lNameSection = document.getElementById('lname-section');
-    const lNameInput = lNameSection.querySelector('input');
-    lNameInput.onfocus = function() {
-        errorFocus(lNameSection);
-    }
-    lNameInput.onblur = function() {
-        validations[1] = errorBlur(lNameSection, lNameInput);
-    }
-
-    const docSection = document.getElementById('doc-section');
-    const docInput = docSection.querySelector('input');
-    docInput.onfocus = function() {
-        errorFocus(docSection);
-    }
-    docInput.onblur = function() {
-        validations[2] = errorBlur(docSection, docInput);
-    }
-
-    const birthSection = document.getElementById('birth-section');
-    const birthInput = birthSection.querySelector('input');
-    birthInput.onfocus = function() {
-        errorFocus(birthSection);
-    }
-    birthInput.onblur = function() {
-        validations[3] = errorBlur(birthSection, birthInput);
-    }
-
-    const phoneSection = document.getElementById('phone-section');
-    const phoneInput = phoneSection.querySelector('input');
-    phoneInput.onfocus = function() {
-        errorFocus(phoneSection);
-    }
-    phoneInput.onblur = function() {
-        validations[4] = errorBlur(phoneSection, phoneInput);
-    }
-
-    const addressSection = document.getElementById('address-section');
-    const addressInput = addressSection.querySelector('input');
-    addressInput.onfocus = function() {
-        errorFocus(addressSection);
-    }
-    addressInput.onblur = function() {
-        validations[5] = errorBlur(addressSection, addressInput);
-    }
-
-    const townSection = document.getElementById('town-section');
-    const townInput = townSection.querySelector('input');
-    townInput.onfocus = function() {
-        errorFocus(townSection);
-    }
-    townInput.onblur = function() {
-        validations[6] = errorBlur(townSection, townInput);
-    }
-
-    const postalSection = document.getElementById('postal-section');
-    const postalInput = postalSection.querySelector('input');
-    postalInput.onfocus = function() {
-        errorFocus(postalSection);
-    }
-    postalInput.onblur = function() {
-        validations[7] = errorBlur(postalSection, postalInput);
-    }
-
-    const emailSection = document.getElementById('email-section');
-    const emailInput = emailSection.querySelector('input');
-    emailInput.onfocus = function() {
-        errorFocus(emailSection);
-    }
-    emailInput.onblur = function() {
-        validations[8] = errorBlur(emailSection, emailInput);
-    }
-
-    const passSection = document.getElementById('pass-section');
-    const passInput = passSection.querySelector('input');
-    passInput.onfocus = function() {
-        errorFocus(passSection);
-    }
-    passInput.onblur = function() {
-        validations[9] = errorBlur(passSection, passInput);
-        if (rPassSection.querySelector('#repeat-label') != null) {
-            errorFocus(rPassSection);
-        }
-        validations[10] = errorRepeat(passSection, rPassSection);
-    }
-
-    const rPassSection = document.getElementById('rpass-section');
-    const rPassInput = rPassSection.querySelector('input');
-    rPassInput.onfocus = function() {
-        errorFocus(rPassSection);
-    }
-    rPassInput.onblur = function() {
-        validations[10] = errorBlur(rPassSection, rPassInput);
-        if (rPassSection.querySelector('.error-label') == null) {
-            validations[9] = errorRepeat(passSection, rPassSection);
-        }
     }
 
     const signButton = document.querySelector('.signup-button');
@@ -343,17 +378,45 @@ window.onload = function () {
             }
         }
         if (finalValidation) {
-            localStorage.setItem('firstName', fNameInput.value);
-            localStorage.setItem('lastName', lNameInput.value);
-            localStorage.setItem('document', docInput.value);
-            localStorage.setItem('birthDate', birthInput.value);
-            localStorage.setItem('phoneNumber', phoneInput.value);
-            localStorage.setItem('address', addressInput.value);
-            localStorage.setItem('town', townInput.value);
-            localStorage.setItem('postalCode', postalInput.value);
-            localStorage.setItem('email', emailInput.value);
-            localStorage.setItem('password', passInput.value);
-            alert('Successfully submited!');
+            var urlWithQP = 'https://basp-m2022-api-rest-server.herokuapp.com/signup?name=' + fNameInput.value
+            + '&lastName=' + lNameInput.value
+            + '&dni=' + docInput.value
+            + '&dob=' + changeDateStructure(birthInput.value)
+            + '&phone=' + phoneInput.value
+            + '&address=' + addressInput.value
+            + '&city=' + townInput.value
+            + '&zip=' + postalInput.value
+            + '&email=' + emailInput.value
+            + '&password=' + passInput.value;
+
+            fetch(urlWithQP)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log('Data:', data);
+                if (data.success) {
+                    localStorage.setItem('firstName', fNameInput.value);
+                    localStorage.setItem('lastName', lNameInput.value);
+                    localStorage.setItem('document', docInput.value);
+                    localStorage.setItem('birthDate', birthInput.value);
+                    localStorage.setItem('phoneNumber', phoneInput.value);
+                    localStorage.setItem('address', addressInput.value);
+                    localStorage.setItem('town', townInput.value);
+                    localStorage.setItem('postalCode', postalInput.value);
+                    localStorage.setItem('email', emailInput.value);
+                    localStorage.setItem('password', passInput.value);
+                    alert(data.msg);
+                }
+                else {
+                    for (i = 0; i < data.errors.length; i++) {
+                        alert(data.errors[i].msg);
+                    }
+                }
+            })
+            .catch(function(error) {
+                console.log('Error:', error);
+            })
         }
         else {
             alert(errorAlert);
